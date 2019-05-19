e16f1ad10417fb62146174ac0a87d4f8863113eb
Franck Brignoli <fbrignoli@gmail.com>
2019-05-19 14:14:18 +0200

    Init

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+brew install zsh git jq awscli aws-shell node yarn 
+
+brew cask install webstorm firefox google-chrome iterm2 docker spectacle vlc macdown freemind dropbox google-drive-file-stream slack spotify tunnelblick zoomus grammarly zwift keepassx licecap
+
